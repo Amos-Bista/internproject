bf5511524580f7aa3858aa983633e366c54c5d28
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2024-08-30 12:46:47 +0545

    main content done

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,37 +1,34 @@
 import React from 'react'
 import Services from './Services'
 import { RiPieChart2Fill } from "react-icons/ri";
+import { FaGlobe } from "react-icons/fa";
+import { FaShoppingBasket } from "react-icons/fa";
 
 const Content = () => {
   return (
-    <main className='flex items-center justify-center p-8'>
-         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl">
-            <div className='md:col-span-2 bg-white p-8'>
-                <h1 className='text-5xl font-bold mb-4'>The Art and Science of Crafting Exceptional Designs</h1>
+    <main className='flex items-center justify-center'>
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-8xl">
+            <div className='md:col-span-2 bg-white px-20 py-12'>
+                <h1 className='text-5xl font-bold mb-4'>The Art and Science <br/> of Crafting <br/>Exceptional Designs</h1>
                 <p className='text-gray-600 mb-6'>Transforming ideas into stellar designs requires a dynamic blend of creativity, technical expertise, and user-focused thinking. </p>
                 <button className='bg-black text-white rounded-full py-2 px-6'>Discover</button>
 
-                {/* <div className='space-y-4'>
-                    <div className='flex items-center'>
-                    <RiPieChart2Fill />
-                    <div>
-                        <h3 className='px-4 mt-6'>Pitch Decks</h3>
-                        <p className='text-gray-500 text-sm mr-2'>The latest trends</p>
-                    </div> 
-                       
-                    </div>
-
-                </div> */}
-                <div className='mt-12 space-y-6'>
-                    <Services title='Pitch Decks' description='The latest trends'/>
-                    <Services title="AI Presentations" description="1000+ examples" />
-                    <Services title="E-commerce" description="Transforming ideas into steller designs" />
+                <div className='mt-10 space-y-4'>
+                    <Services icon={<RiPieChart2Fill />}
+                     title='Pitch Decks' description='The latest trends'/>
+                    <Services icon={< FaGlobe/>}
+                     title="AI Presentations" description="1000+ examples" />
+                    <Services icon={<FaShoppingBasket/>}
+                     title="E-commerce" description="Transforming ideas into" />
                 </div>
+            </div>
+            
+            <div className='flex flex-col justify-center items-center bg-gray-100 h-full w-full'>
+                <div className='relative w-full h-full'>
+                    </div>
 
             </div>
         </div>
-        
-
 
     </main>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { RiPieChart2Fill } from "react-icons/ri";
+import { FaArrowRightLong } from "react-icons/fa6";
 
-const Services = ({title, descriptions}) => {
+const Services = ({icon, title, description}) => {
   return (
-    <div className="flex items-center justify-between border-b border-gray-300 pb-4">
-        <div>
-
+    <div className="flex items-center justify-between border-b border-gray-300 pb-4 w-6/12">
+        <div className='flex items-center space-x-4'>
+          <span>{icon}</span>
+          <div>
+        <h3>{title}</h3>
+        <p className='text-sm text-gray-600'>{description}</p>
         </div>
+        </div>
+        <span>< FaArrowRightLong/></span>
 
     </div>
   )
